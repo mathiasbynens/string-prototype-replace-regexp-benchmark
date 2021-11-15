58ff4fef656757a95aacf807f8a3b9df54a6b24b
Mathias Bynens <mathias@qiwi.be>
2021-11-15 12:21:52 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# `string-prototype-replace-regexp-benchmark`
+
+This project generates benchmarks for `String.prototype.{replace,replaceAll}` with global regular expressions based on [`emoji-test-regex-pattern`](https://github.com/mathiasbynens/emoji-test-regex-pattern).
+
+See [the `dist/` folder](https://github.com/mathiasbynens/string-prototype-replace-regexp-benchmark/tree/main/dist).
---
+++ build.js
@@ -0,0 +1,50 @@
+// Copyright 2021 Google Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the “License”);
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// <https://apache.org/licenses/LICENSE-2.0>.
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an “AS IS” BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+// implied. See the License for the specific language governing
+// permissions and limitations under the License.
+
+const fs = require('fs');
+const jsesc = require('jsesc');
+
+const EMOJI_TEST_PATH = './node_modules/emoji-test-regex-pattern/dist/latest';
+const index = fs.readFileSync(`${EMOJI_TEST_PATH}/index.txt`, 'utf8').toString().trim();
+const pattern = fs.readFileSync(`${EMOJI_TEST_PATH}/javascript.txt`, 'utf8').toString().trim();
+const patternU = fs.readFileSync(`${EMOJI_TEST_PATH}/javascript-u.txt`, 'utf8').toString().trim();
+
+const toStringLiteral = (string) => {
+  // We’d have used JSON.stringify() if it were ASCII-safe.
+  return jsesc(string, { wrap: true });
+};
+
+const generateSource = ({ method, flags }) => {
+  const id = `${ method }-${ flags }`;
+  return `
+const re = /${ pattern }/${ flags };
+const string = ${ toStringLiteral(index) };
+console.time(${ toStringLiteral(id) });
+const result = string.${ method }(re, (match) => {
+  return '_';
+});
+console.timeEnd(${ toStringLiteral(id) });
+`.trim() + '\n';
+};
+
+const writeFile = ({ method, flags }) => {
+  fs.writeFileSync(
+    `./dist/${method}-${flags}.js`,
+    generateSource({ method, flags })
+  );
+};
+
+writeFile({ method: 'replace',    flags: 'g' });
+writeFile({ method: 'replace',    flags: 'gu' });
+writeFile({ method: 'replaceAll', flags: 'g' });
+writeFile({ method: 'replaceAll', flags: 'gu' });
